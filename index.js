c5f48e2e6a7293d3dde31db0f6eda51673ed5124
Lucas da Silva Maciel <lucas.silva@olostech.com>
2023-10-02 18:34:30 -0300

    Selects

--- index.js
+++ index.js
@@ -24,3 +24,25 @@ database.insert(data).into("games").then(data => {
 	console.log(err);
 });
 */
+
+
+/*
+SELECTS
+database.select("*").table("games").then(data => {
+	console.log(data)
+}).catch(err => {
+	console.log(err);
+});
+
+
+database.insert({name: "Mists of noyah", price: 40.34}).into("games").then(data => {
+	database.select("*").table("games").then(data => {
+		console.log(data)
+	}).catch(err => {
+		console.log(err);
+	})
+}).catch(err => {
+	console.log(err);
+});
+
+*/

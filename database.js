3dfee2caef6f777b7c8d7ce3e7a445c4a2c4fd5e
Lucas da Silva Maciel <lucas.silva@olostech.com>
2023-10-02 18:28:44 -0300

    Insert

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const knex = require('knex')({
-	client: 'mysql',
+	client: 'mysql2',
 	connection: {
 	  host : '127.0.0.1',
 	  port : 3306,
--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 var database = require("./database");
+
+/*
+INSERT
+var data = [
+	{
+		name: "Call of Duty 2",
+		price: 30.67
+	},
+	{
+		name: "GTA",
+		price: 30.67
+	},
+	{
+		name: "Sonic",
+		price: 30.67
+	}
+]
+
+
+database.insert(data).into("games").then(data => {
+	console.log(data)
+}).catch(err => {
+	console.log(err);
+});
+*/
